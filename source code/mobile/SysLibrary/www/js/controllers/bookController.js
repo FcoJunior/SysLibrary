2c0a48f691f3d1ac433280d13c34fe01a89589d3
FcoJunr <fcojunr@gmail.com>
2015-10-25 18:24:37 -0300

    Refresh mobile

--- source code/mobile/SysLibrary/www/js/controllers/bookController.js
+++ source code/mobile/SysLibrary/www/js/controllers/bookController.js
@@ -4,9 +4,18 @@
 app.
   controller('BookCtrl',['$scope', '$http', 'config', function($scope, $http, config){
     $scope.books = [];
+
     $http.get(config.server + '/Obra')
       .success(function(data, statusCode){
         $scope.books = data.Obras.$values;
       });
 
+    $scope.refresh = function(){
+      $http.get(config.server + '/Obra')
+        .success(function(data, statusCode){
+          $scope.books = data.Obras.$values;
+        }).finally(function(){
+          $scope.$broadcast('scroll.refreshComplete');
+        });
+    }
   }]);
