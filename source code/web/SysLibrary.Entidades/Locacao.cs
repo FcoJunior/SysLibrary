5f7bf5425a4beafe823eaf534bc4fcbce200185e
FcoJunr <fcojunr@gmail.com>
2015-11-16 21:18:28 -0300

    Locação

---
+++ source code/web/SysLibrary.Entidades/Locacao.cs
@@ -0,0 +1,17 @@
+﻿using SysLibrary.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SysLibrary.Entities
+{
+    public class Locacao : BaseEntity
+    {
+        public int UsuarioId { get; set; }
+        public int ExemplarId { get; set; }
+        public DateTime DataDeLocacao { get; set; }
+        public DateTime DataDeDevolucao { get; set; }
+    }
+}
---
+++ source code/web/SysLibrary.WebApp/Controllers/App/LocacaoController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SysLibrary.Entities;
+using SysLibrary.Data;
+using SysLibrary.Business;
+using SysLibrary.WebApp.Models;
+using SysLibrary.Util;
+using System.Web.Security;
+
+namespace SysLibrary.WebApp.Controllers.App
+{
+    [Authorize]
+    public class LocacaoController : Controller
+    {
+        [HttpGet]
+        public ActionResult Select(int? id)
+        {
+            return View(UsuarioBO.GetAllActive<Usuario>());
+        }
+
+        // GET: Locacao
+        public ActionResult Index(int usuarioId)
+        {
+            return View();
+        }
+
+        // GET: Locacao/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Locacao/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Locacao/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Locacao/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Locacao/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Locacao/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Locacao/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
